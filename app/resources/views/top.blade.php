92d323e6ec49320d8aad59b764477474b76c01ef
tadashimasuda <tmasu4260@gmail.com>
2020-03-21 20:38:30 +0900

    image show

--- app/resources/views/top.blade.php
+++ app/resources/views/top.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,6 +9,7 @@
     <h1>hello world</h1>
     @foreach($tasks as $task)
         <p>{{ $task->content }}</p>
+        <img src="/image/{{ $task->imgpath }}" alt="">
     @endforeach
 </body>
 </html>
